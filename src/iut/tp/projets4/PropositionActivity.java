545623a9bef30d98799feebda177f933dc8b8dca
ValentinArg <valentin.argenty@gmail.com>
2017-03-08 18:44:17 +0100

    ddd

--- src/iut/tp/projets4/PropositionActivity.java
+++ src/iut/tp/projets4/PropositionActivity.java
@@ -4,31 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
-
-public class PlatActivity extends Activity {
-	
-	private int TAILLE_MAX;
+public class PropositionActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_plat);
-		
-		String[] tabEntree = new String[TAILLE_MAX];
-		tabEntree[0] = "Fromage";
-		tabEntree[1] = "Salade";
-        Spinner spinnerEntree = (Spinner) findViewById(R.id.spinnerEntree);
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, tabEntree);
-        spinnerEntree.setAdapter(adapter);
+		setContentView(R.layout.activity_proposition);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.proposition, menu);
 		return true;
 	}
 
@@ -38,7 +26,7 @@ public class PlatActivity extends Activity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_apropos) {
+		if (id == R.id.action_settings) {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
