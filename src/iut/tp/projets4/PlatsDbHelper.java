53048bf76e40a5f7d5330acc5eaf8034b58d9fe7
Matra <Matra@172.17.52.6>
2017-03-10 08:55:51 +0100

    aze

--- src/iut/tp/projets4/PlatsDbHelper.java
+++ src/iut/tp/projets4/PlatsDbHelper.java
@@ -9,10 +9,25 @@ public class PlatsDbHelper extends SQLiteOpenHelper{
 		public static final int DATABASE_VERSION = 1;
 		public static final String DATABASE_NAME="plats.db";
 		
-		public final String SQL_CREATE = "CREATE TABLE Plats(id INTEGER PRIMARY KEY, nom TEXT, calories INTEGER, type TEXT);"
-				+ " INSERT INTO Plats VALUES(1,'Tagliatelles',300, 'Plat');";
+		public final String SQL_CREATE = "CREATE TABLE Plats(id INTEGER PRIMARY KEY, nom TEXT, calories INTEGER, portion INTEGER ,type TEXT);"
+				+ " INSERT INTO Plats VALUES(1,'Tagliatelles', 215 ,115, 'Plat');"
+				+ " INSERT INTO Plats VALUES(2,'Blanquette de veau', 548 ,400 , 'Plat');"
+				+ " INSERT INTO Plats VALUES(3,'Kebab', 740  , 360 'Plat');" 
+				+ " INSERT INTO Plats VALUES(4,'Pizza', 272  , 102 , 'Plat');" 
+				+ " INSERT INTO Plats VALUES(5,'Cassoulet', 487 ,420 , 'Plat');" 
+				+ " INSERT INTO Plats VALUES(6,'Burrito', 326  , 200, 'Plat');" 
+				+ " INSERT INTO Plats VALUES(7,'Choucroute', 142  , 200, 'Plat');" 
+				+ " INSERT INTO Plats VALUES(8,'Big Mac', 561  ,219 , 'Plat');" 
+				+ " INSERT INTO Plats VALUES(9,'Tomate Cerise', 100 , 100, 'Entree');" 
+				+ " INSERT INTO Plats VALUES(10,'Salade de thon', 383  , 205, 'Entree');" 
+				+ " INSERT INTO Plats VALUES(11,'Saumon', 367 , 178, 'Entree');"
+				+ " INSERT INTO Plats VALUES(12,'Glace au chocolat', 157 , 72, 'Dessert');" 
+				+ " INSERT INTO Plats VALUES(13,'Gauffre', 103 , 33, 'Dessert');" 
+				+ " INSERT INTO Plats VALUES(14,'Tarte au pomme', 290 , 150, 'Dessert');" ;
+				
 		public final String SQL_DELETE = "DROP TABLE IF EXISTS Plats;";
 		
+	
 		public PlatsDbHelper(Context context){
 			super(context, DATABASE_NAME, null, DATABASE_VERSION);
 		}
